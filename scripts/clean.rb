b2a9c3a217b58c42f422f66683f8f5a2bcef85ee
kumagi <hiroki.kumazaki@gmail.com>
2025-06-08 10:43:32 +0900

    upate dict

--- scripts/clean.rb
+++ scripts/clean.rb
@@ -4,12 +4,13 @@ words = Dir.glob("*", base: "dict/en/ja").sort
 exists = words.clone.select {|w|
   File.exist?("dict/en/ja/#{w}/data.json")
 }
+
 exists.each{|w|
-  result = JSON.parse(File.open("dict/en/ja/#{w}/data.json").read)
   begin
+    result = JSON.parse(File.open("dict/en/ja/#{w}/data.json").read)
     result['word']
   rescue
     puts "cannot read #{w}"
-    `rm -f dict/en/ja/#{w}/data.json`
+    `rm -rf dict/en/ja/#{w}/data.json`
   end
 }
