cbffe4f2667eb8f176939c6fa919ecc8a247c179
kumagi <hiroki.kumazaki@gmail.com>
2025-06-10 02:28:26 +0900

    support mobile

--- scripts/render.ts
+++ scripts/render.ts
@@ -8,10 +8,62 @@ function parseMarkdownBold(text: string) {
     return text.replace(/\*\*(.*?)\*\*/g, '<strong>$1</strong>');
 }
 
+function translatePartOfSpeech(pos: string): string {
+    switch (pos) {
+	case "verb":
+	    return "動詞";
+	case "verb (vt)":
+	    return "他動詞";
+	case "verb (vi)":
+	    return "自動詞"
+	case "noun":
+	    return "名詞";
+	case "pronoun":
+	    return "代名詞";
+	case "adjective":
+	    return "形容詞";
+	case "adverb":
+	    return "副詞";
+	case "preposition":
+	    return "前置詞";
+	case "conjunction":
+	    return "接続詞";
+	case "interjection":
+	    return "間投詞";
+    }
+    return "Unknown";
+}
+
+function translateTransitivity(en: string): string {
+    switch (en) {
+	case "vr":
+	case "vi":
+	    return "自";
+	case "vt":
+	    return "他";
+    }
+    return "sss";
+}
+
+function mustachePreprocess(data: WordData): WordData {
+    for (let i = 0; i < data.meanings.length; i++) {
+	data.meanings[i].index_plus_1 = i + 1;
+	data.meanings[i].part_of_speech_translated = translatePartOfSpeech(data.meanings[i].part_of_speech);
+	if (data.meanings[i].transitivity) {
+	    data.meanings[i].transitivity_translated = translateTransitivity(data.meanings[i].transitivity);
+	}
+    }
+    for (let i = 0; i < data.example_sentences.length; i++) {
+	data.example_sentences[i].type_translated = translatePartOfSpeech(data.example_sentences[i].type);
+	data.example_sentences[i].sentence = parseMarkdownBold(data.example_sentences[i].sentence)
+    }
+    data.readability_explanation.text = parseMarkdownBold(data.readability_explanation.text);
+    data.usage_notes.explanation = parseMarkdownBold(data.usage_notes.explanation)
+    return data;
+}
+
 export function render(data: WordData[]): string {
-    data[0].readability_explanation.text = parseMarkdownBold(data[0].readability_explanation.text);
-    data[0].usage_notes.explanation = parseMarkdownBold(data[0].usage_notes.explanation)
-    return template.render(data[0]);
+    return template.render(mustachePreprocess(data[0]));  // TODO: Handle multiple words data if exists.
 }
 
 export function renderNotfoundMessage(target: string, words: string[]): string {
--- scripts/word_data.ts
+++ scripts/word_data.ts
@@ -1,4 +1,18 @@
 
+interface Meanings {
+    index_plus_1: number;
+    part_of_speech: string;
+    part_of_speech_translated: string;
+    transitivity: string;
+    transitivity_translated: string;
+};
+
+interface ExampleSentence {
+    type: string;
+    type_translated: string;
+    sentence: string;
+}
+
 export interface WordData {
     readability_explanation: {
 	text: string
@@ -6,4 +20,7 @@ export interface WordData {
     usage_notes: {
 	explanation: string
     };
+    meanings:  Meanings[];
+    example_sentences: ExampleSentence[];
 };
+
